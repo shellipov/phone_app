6342595f97204a56b326fde88f1e15fa301faefa
shellipov <shellipov@mail.ru>
2021-11-09 19:56:48 +0300

    ignore remote dubugger

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import AppRouter from './src/AppRouter'
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['Remote debugger']);
 
 
 export default function App() {
